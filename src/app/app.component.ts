0e180a2d32e9fbdcb29b2184c7774d86697e485e
jananigel <jananigel@hotmail.com>
2021-04-05 01:01:43 +0800

    feat: add simple drag element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { fromEvent } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 const IMAGE_URL = "imageUrl";
 
@@ -20,6 +22,16 @@ export class AppComponent {
 
   onDrop(files: FileList): void {
     this.fileHandle(files);
+  }
+
+  onMouseDown(item, mouseDown: MouseEvent) {
+    fromEvent(item, "mousemove")
+      .pipe(takeUntil(fromEvent(item, "mouseup")))
+      .subscribe((event: MouseEvent) => {
+        item.style.position = "absolute";
+        item.style.left = event.clientX - mouseDown.offsetX + "px";
+        item.style.top = event.clientY - mouseDown.offsetY + "px";
+      });
   }
 
   private fileHandle(fileList: FileList) {
